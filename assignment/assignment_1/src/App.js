b038ede8b2d1a8937eca2a0e566198661a887815
Abhinav Kumar Arya <91782639+AbhinavArya007@users.noreply.github.com>
2022-02-24 01:49:53 +0530

    Add files via upload

---
+++ assignment/assignment_1/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Function from "./Component/Function";
+import Class from "./Component/Class";
+import "./styles.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="continer">
+        <div className="header">
+          <h1 className="title">
+            Functional and Class Component
+          </h1>
+        </div>
+        <div className="box-continer">
+          <Function />
+          <Class />
+        </div>
+      </div>
+    </>
+  );
+};
+export default App;
